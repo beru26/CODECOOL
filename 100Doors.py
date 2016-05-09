fc2ed3db300b54101f24c1fc573f1757e4d017e7
beru26 <berkes.edina@gmail.com>
2016-05-09 11:42:34 +0200

    szazajto

---
+++ 100Doors.py
@@ -0,0 +1,16 @@
+range(1,100)
+i = 1
+
+for i in range(1,11):
+    print ("%s th is open") % i**2
+
+# if i <= 100:
+#     for i in range:
+#         if i%n == 0:
+#             [i] = 1
+#             i = i + 1
+#             n = n + 1
+#
+#         else i in mylist:
+#             i = i + 1
+#             n= n + 1
